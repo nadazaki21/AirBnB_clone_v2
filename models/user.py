c23f6df13024c3509c0c8e524dad80974d779a0d
nadazkai21 <nadazaki019@gmail.com>
2024-04-18 03:45:01 +0200

    verison became malfunctioning

--- models/user.py
+++ models/user.py
@@ -6,29 +6,18 @@ from sqlalchemy.orm import relationship
 from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv()
-
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
 
-        places = relationship(
-            "Place", back_populates="user", cascade="all, delete-orphan"
-        )
-        reviews = relationship(
-            "Review", back_populates="user", cascade="all, delete-orphan"
-        )
-
-    else:
-
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    places = relationship("Place", back_populates="user", cascade="all, delete-orphan")
+    reviews = relationship(
+        "Review", back_populates="user", cascade="all, delete-orphan"
+    )
