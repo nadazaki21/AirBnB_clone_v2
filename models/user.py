abb648788ed882aaa4615d8505c65141eb3b9ec0
nadazkai21 <nadazaki019@gmail.com>
2024-04-18 02:48:03 +0200

    trying to fix 8

--- models/user.py
+++ models/user.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    """This class defines a user by various attributes"""
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+
+        places = relationship(
+            "Place", back_populates="user", cascade="all, delete-orphan"
+        )
+        reviews = relationship(
+            "Review", back_populates="user", cascade="all, delete-orphan"
+        )
+
     else:
+
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
