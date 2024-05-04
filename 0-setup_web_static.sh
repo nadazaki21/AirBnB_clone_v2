735e51c5fd862467d1df1f2341933d14b3923cc6
nadazkai21 <nadazaki019@gmail.com>
2024-05-04 03:58:20 +0300

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,6 +14,6 @@ echo '<html>
   </body>
 </html>' > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i "45i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
